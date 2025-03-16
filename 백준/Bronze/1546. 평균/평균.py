eb7c7019e818d0159b8abcf947859b0a1fe8d173
ibtSdan <ibtsdan@gmail.com>
2025-03-16 11:06:28 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,7 +1,4 @@
 n = int(input())
 lst = list(map(int, input().split()))
-lst_max = max(lst)
-m = 0
-for i in lst:
-    m += (i/lst_max)*100
-print(m/n)
+mymax = max(lst)
+print(sum(lst)/mymax*100/len(lst))
