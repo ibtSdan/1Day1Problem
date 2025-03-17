11ffad8be6a1f8150d2f709dbdf22220bdb00f38
ibtSdan <ibtsdan@gmail.com>
2025-03-17 17:24:24 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+n, m = map(int, input().split())
+A = [list(map(int, input())) for _ in range(n)]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def BFS(x,y):
+    dq = deque()
+    dq.append((x,y))
+    
+    while dq:
+        x, y = dq.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0<=nx<n and 0<=ny<m and A[nx][ny]==1:
+                A[nx][ny] = A[x][y] + 1
+                dq.append((nx,ny))
+    print(A[n-1][m-1])
+
+BFS(0,0)
