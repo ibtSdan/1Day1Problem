7dca189395c40a0b4f697476d08c11956614df2a
ibtSdan <ibtsdan@gmail.com>
2025-03-21 16:27:48 +0900

    [Gold V] Title: 신기한 소수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,25 +1,20 @@
-import sys, math
+import sys
 sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
-n = int(input())
 
-# DFS
-def DFS(number):
-    if len(str(number)) == n:
-        print(number)
-    else:
-        for i in range(1,10,2):
-            if isPrime(number*10+i):
-                DFS(number*10+i)
-
-# 소수 판별
-def isPrime(num):
-    for i in range(2,int(math.sqrt(num)+1)):
-        if num%i==0:
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
             return False
     return True
 
-
+def DFS(n):
+    if len(str(n))==N:
+        print(n)
+    for i in range(1,10,2):
+        if isPrime(n*10+i):
+            DFS(n*10+i)
+    
+N = int(input())    
 DFS(2)
 DFS(3)
 DFS(5)
