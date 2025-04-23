75cebd2ac9484735c7c545031e023d0521dfddaa
ibtSdan <ibtsdan@gmail.com>
2025-04-23 11:23:25 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,12 +1,13 @@
-s = list(input().split('-'))
-ans = 0
-def ssum(a):
-  lst = list(map(int, a.split('+')))
-  return sum(lst)
+import sys
+input = sys.stdin.readline
 
-for i in range(len(s)):
-  if i==0:
-    ans += ssum(s[i])
-  else:
-    ans -= ssum(s[i])
-print(ans)
+n = list(input().split('-'))
+ne = 0
+
+po = sum(list(map(int, n[0].split('+'))))
+
+for i in range(1,len(n)):
+    lst = list(map(int, n[i].split('+')))
+    ne += sum(lst)
+        
+print(po - ne)
