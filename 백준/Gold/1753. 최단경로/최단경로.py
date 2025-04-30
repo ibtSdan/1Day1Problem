5b500aeca25ccef847982baee6e523697ba40e07
ibtSdan <ibtsdan@gmail.com>
2025-04-30 21:24:54 +0900

    [Gold IV] Title: 최단경로, Time: 608 ms, Memory: 69516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,31 +1,30 @@
-import sys
-import heapq
-
+import sys, heapq
 input = sys.stdin.readline
+
+INF = int(1e9)
 n, m = map(int, input().split())
 start = int(input())
 A = [[] for _ in range(n+1)]
 for _ in range(m):
     u, v, w = map(int, input().split())
     A[u].append((v,w))
-    
-distance = [int(1e9)] * (n+1)
+
+total = [INF] * (n+1)
 hq = []
 heapq.heappush(hq, (0, start))
-distance[start] = 0
-
+total[start] = 0
 while hq:
-    dis, now = heapq.heappop(hq)
-    if dis > distance[now]:
+    distance, next = heapq.heappop(hq)
+    if distance > total[next]:
         continue
-    for vv, ww in A[now]:
-        cost = ww + dis
-        if cost < distance[vv]:
-            distance[vv] = cost
-            heapq.heappush(hq, (cost, vv))
+    for edge, weight in A[next]:
+        cost = total[next] + weight
+        if cost < total[edge]:
+            total[edge] = cost
+            heapq.heappush(hq, (cost, edge))
 
 for i in range(1,n+1):
-    if distance[i] == int(1e9):
+    if total[i] == INF:
         print("INF")
     else:
-        print(distance[i])
+        print(total[i])
