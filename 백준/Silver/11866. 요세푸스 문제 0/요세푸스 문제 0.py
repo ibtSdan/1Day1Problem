330192697ec1c45e306ee23db0fdf8af90ce0229
ibtSdan <ibtsdan@gmail.com>
2025-06-28 13:23:49 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 84 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,8 +1,6 @@
 from collections import deque
-
-n, k = map(int, input().split())
-lst = list(i for i in range(1, n+1))
-dq = deque(lst)
+n, k = map(int,input().split())
+dq = deque([i for i in range(1,n+1)])
 ans = []
 
 while dq:
@@ -10,7 +8,5 @@ while dq:
         dq.append(dq.popleft())
     ans.append(dq.popleft())
 
-print('<', end='')
-for i in range(len(ans)-1):
-    print(ans[i], end=', ')
-print(str(ans[-1])+'>')
+answer = ', '.join(str(i) for i in ans)
+print("<"+answer+">")
