5cf1d0f4b92116630ca2d2b6542df5b285ef667b
ibtSdan <ibtsdan@gmail.com>
2025-06-29 15:31:09 +0900

    [Silver V] Title: 나이순 정렬, Time: 148 ms, Memory: 37528 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -3,11 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 lst = [[] for _ in range(201)]
+
 for _ in range(n):
     age, name = input().split()
     lst[int(age)].append(name)
 
-for i in range(201):
+for i in range(1,201):
     if lst[i]:
-        for j in lst[i]:
-            print(i, j)
+        for name in lst[i]:
+            print(str(i)+" "+name)
