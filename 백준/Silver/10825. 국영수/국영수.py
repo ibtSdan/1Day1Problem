aa3c91c77ef5fe5c52affecbdd88e7f9454ad40d
ibtSdan <ibtsdan@gmail.com>
2025-06-30 14:19:00 +0900

    [Silver IV] Title: 국영수, Time: 328 ms, Memory: 63432 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,13 +1,14 @@
 import sys
 input = sys.stdin.readline
 
+
 n = int(input())
-lst = []
+A = []
 for _ in range(n):
     name, kor, eng, math = input().split()
-    lst.append((name, int(kor), int(eng), int(math)))
+    A.append((name, int(kor), int(eng), int(math)))
 
-lst.sort(key = lambda x : (-x[1], x[2], -x[3], x[0]))
+A.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
 
-for name in lst:
+for name in A:
     print(name[0])
