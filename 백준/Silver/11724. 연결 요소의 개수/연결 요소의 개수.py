02d88fb47361ec4caee006073a8cd2911c5d368d
ibtSdan <ibtsdan@gmail.com>
2025-07-04 22:08:03 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 568 ms, Memory: 66080 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,25 @@
 import sys
+input= sys.stdin.readline
 sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
-n, m = map(int, input().split())
-A = [[] for _ in range(n+1)]
-visited = [False]*(n+1)
 
-# 그래프 입력 받기
+n, m = map(int,input().split())
+A = [[] for _ in range(n+1)]
 for _ in range(m):
-    u, v = map(int, input().split())
+    u, v = map(int,input().split())
     A[u].append(v)
     A[v].append(u)
-
-# DFS
+    
 def DFS(v):
     visited[v] = True
     for i in A[v]:
         if not visited[i]:
             DFS(i)
 
-cnt = 0
+total = 0
+visited = [False]*(n+1)
 for i in range(1,n+1):
     if not visited[i]:
+        total += 1
         DFS(i)
-        cnt += 1
 
-print(cnt)
+print(total)
