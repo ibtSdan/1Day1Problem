e50bd17e22ddde86c1e39160c82cfe762d056d97
ibtSdan <ibtsdan@gmail.com>
2025-07-04 21:39:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 
 n = int(input())
 A = []
@@ -13,27 +12,24 @@ visited = [[False for _ in range(n)] for _ in range(n)]
 total = 0
 ans = []
 
-def BFS(i, j):
-    dq = deque()
-    visited[i][j] = True
-    dq.append((i,j))
-    cnt = 1
-    while dq:
-        now = dq.popleft()
-        for k in range(4):
-            x = now[0] + dx[k]
-            y = now[1] + dy[k]
-            if 0<=x<n and 0<=y<n and not visited[x][y] and A[x][y]==1:
-                visited[x][y] = True
-                dq.append((x,y))
-                cnt += 1
-    ans.append(cnt)
+def DFS(x, y):
+    global cnt
+    visited[x][y] = True
+    cnt += 1
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+        if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and A[nx][ny]==1:
+            DFS(nx, ny)
+    
 
 for i in range(n):
     for j in range(n):
         if not visited[i][j] and A[i][j]==1:
             total += 1
-            BFS(i,j)
+            cnt = 0
+            DFS(i,j)
+            ans.append(cnt)
 
 ans.sort()
 print(total)
