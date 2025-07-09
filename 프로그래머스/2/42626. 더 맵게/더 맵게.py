9616c9a172037f207ef860abd932f7a5345436b2
ibtSdan <ibtsdan@gmail.com>
2025-07-09 18:46:11 +0900

    [level 2] Title: 더 맵게, Time: 2401.00 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,16 +1,21 @@
 import heapq
-def solution(scoville, K):
-    heapq.heapify(scoville)
-    ans = 0
-    
-    while any(i < K for i in scoville):
-        if len(scoville) < 2:
-            return -1
-        
-        first = heapq.heappop(scoville)
-        second = heapq.heappop(scoville)
-        new = first + second*2
-        heapq.heappush(scoville, new)
-        ans += 1
 
-    return ans
+def solution(scoville, k):
+    heapq.heapify(scoville)
+    check = True
+    cnt = 0
+    
+    while any(i < k for i in scoville):
+        if len(scoville)==1:
+            check = False
+            break
+        else:
+            first = heapq.heappop(scoville)
+            second = heapq.heappop(scoville)
+            heapq.heappush(scoville, first+second*2)
+            cnt += 1
+            
+    if check:
+        return cnt
+    else:
+        return -1
