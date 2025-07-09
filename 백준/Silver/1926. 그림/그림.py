22b63fcc6f3b746257f030c1d9919d3530a4ef30
ibtSdan <ibtsdan@gmail.com>
2025-07-09 19:01:47 +0900

    [Silver I] Title: 그림, Time: 272 ms, Memory: 36752 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,35 +1,40 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n, m = map(int,input().split())
 A = []
 for _ in range(n):
-    lst = list(map(int,input().split()))
-    A.append(lst)
+    A.append(list(map(int, input().split())))
+
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
-visited = [[False for _ in range(m)] for _ in range(n)]
-ans = []
-    
-def DFS(x,y):
-    global cnt
-    cnt += 1
-    visited[x][y] = True
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and A[nx][ny]==1:
-            DFS(nx,ny)
-
 total = 0
+max_area = 0
+visited = [[False for _ in range(m)] for _ in range(n)]
+
+def BFS(i,j):
+    global max_area
+    dq = deque()
+    dq.append((i,j))
+    visited[i][j] = True
+    area = 1
+    while dq:
+        now = dq.popleft()
+        for k in range(4):
+            x = dx[k] + now[0]
+            y = dy[k] + now[1]
+            if 0<=x<n and 0<=y<m and not visited[x][y] and A[x][y]==1:
+                dq.append((x,y))
+                area += 1
+                visited[x][y] = True
+    max_area = max(max_area, area)
+    
 for i in range(n):
     for j in range(m):
         if not visited[i][j] and A[i][j]==1:
+            BFS(i,j)
             total += 1
-            cnt = 0
-            DFS(i,j)
-            ans.append(cnt)
             
 print(total)
-print(max(ans) if ans else 0)
+print(max_area)
