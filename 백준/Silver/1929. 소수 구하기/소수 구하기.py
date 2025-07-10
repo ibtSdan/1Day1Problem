6245c53a8c1ba815d3144b52c19d72e888e72206
ibtSdan <ibtsdan@gmail.com>
2025-07-10 10:15:27 +0900

    [Silver III] Title: 소수 구하기, Time: 432 ms, Memory: 71828 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,14 @@
-a,b = map(int,input().split())
-A = [0] * (b+1)
-for i in range(2,b+1):
+n, m = map(int, input().split())
+A = [0]*(m+1)
+for i in range(2, m+1):
     A[i] = i
-
-for i in range(2, int(b**0.5)+1):
-    if A[i] == 0:
+    
+for i in range(2,int(m**0.5)+1):
+    if A[i]==0:
         continue
-    for j in range(i*2, b+1, i):
+    for j in range(i*2,m+1,i):
         A[j] = 0
-        
-for i in range(a,b+1):
-    if A[i]:
-        print(A[i])
+
+for i in A:
+    if i and i>=n:
+        print(i)
