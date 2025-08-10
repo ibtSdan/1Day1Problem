d6ed002ed98666cacd12ad5fac7f3df6de9f3390
ibtSdan <ibtsdan@gmail.com>
2025-08-10 22:32:51 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,21 +1,16 @@
 n = int(input())
-d = [[0 for j in range(10)] for i in range(n+1)]
-MOD = 1000000000
+dp = [[0]*10 for _ in range(n+1)]
 
 for i in range(1,10):
-    d[1][i] = 1
-
+    dp[1][i] = 1
+    
 for i in range(2,n+1):
-    for j in range(10):
-        if j==0:
-            d[i][j] = d[i-1][1]
-        elif j==9:
-            d[i][j] = d[i-1][8]
-        else:
-            d[i][j] = (d[i-1][j-1] + d[i-1][j+1]) % MOD
-            
+    dp[i][0] = dp[i-1][1]
+    dp[i][9] = dp[i-1][8]
+    for j in range(1,9):
+        dp[i][j] = dp[i-1][j+1] + dp[i-1][j-1]
+        
 cnt = 0
 for i in range(10):
-    cnt += d[n][i]
-    
-print(cnt % MOD)
+    cnt += dp[n][i]
+print(cnt%1000000000)
