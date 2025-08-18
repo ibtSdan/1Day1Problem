0f41ec69ee0af284d9def5bfb8a356216e31f09e
ibtSdan <ibtsdan@gmail.com>
2025-08-18 15:14:26 +0900

    [Silver II] Title: 연속합, Time: 76 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,12 +1,8 @@
 n = int(input())
 lst = list(map(int, input().split()))
 
-d = [0]*n
-d[0] = lst[0]
-ans = lst[0]
-
+dp = [0]*n
+dp[0] = lst[0]
 for i in range(1,n):
-    d[i] = max(d[i-1]+lst[i], lst[i])
-    ans = max(ans, d[i])
-    
-print(ans)
+    dp[i] = max(dp[i-1]+lst[i], lst[i])
+print(max(dp))
