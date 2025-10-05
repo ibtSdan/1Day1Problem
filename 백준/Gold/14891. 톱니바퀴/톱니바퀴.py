00dccacf33a7d10fe0a46fb9a9a5c99d4efcb399
ibtSdan <ibtsdan@gmail.com>
2025-10-05 19:03:42 +0900

    [Gold V] Title: 톱니바퀴, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,4 +1,5 @@
 lst = [0]
+n_gears = 4
 for _ in range(4):
     lst.append(list(map(int,input().strip())))
 
@@ -10,51 +11,29 @@ def re(lst):
 
 test = int(input())
 for _ in range(test):
-    n, di = map(int,input().split())
-    check = [0]*5
-    check[n] = di
-    if n==1:
-        if lst[n][2] != lst[n+1][6]:
-            check[n+1] = check[n]*-1
-        if lst[n+1][2] != lst[n+2][6]:
-            check[n+2] = check[n+1]*-1
-        if lst[n+2][2] != lst[n+3][6]:
-            check[n+3] = check[n+2]*-1
-    elif n==2:
-        if lst[n][6] != lst[n-1][2]:
-            check[n-1] = check[n]*-1
-        if lst[n][2] != lst[n+1][6]:
-            check[n+1] = check[n]*-1
-        if lst[n+1][2] != lst[n+2][6]:
-            check[n+2] = check[n+1]*-1
-    elif n==3:
-        if lst[n][6] != lst[n-1][2]:
-            check[n-1] = check[n]*-1
-        if lst[n-1][6] != lst[n-2][2]:
-            check[n-2] = check[n-1]*-1
-        if lst[n][2] != lst[n+1][6]:
-            check[n+1] = check[n]*-1
-    else:
-        if lst[n][6] != lst[n-1][2]:
-            check[n-1] = check[n]*-1
-        if lst[n-1][6] != lst[n-2][2]:
-            check[n-2] = check[n-1]*-1
-        if lst[n-2][6] != lst[n-3][2]:
-            check[n-3] = check[n-2]*-1
+    gear, direction = map(int, input().split())
+    check = [0]*(n_gears+1)
+    check[gear] = direction
 
-    for i in range(1,5):
+    for i in range(gear,n_gears):
+        if lst[i][2] != lst[i+1][6]:
+            check[i+1] = -check[i]
+        else:
+            break
+    for i in range(gear,1,-1):
+        if lst[i][6] != lst[i-1][2]:
+            check[i-1] = -check[i]
+        else:
+            break
+
+    for i in range(1,n_gears+1):
         if check[i]==1:
             lst[i] = clock(lst[i])
         elif check[i]==-1:
             lst[i] = re(lst[i])
 
-ans = 0  
-if lst[1][0]==1:
-    ans += 1
-if lst[2][0]==1:
-    ans += 2
-if lst[3][0]==1:
-    ans += 4
-if lst[4][0]==1:
-    ans += 8
+ans = 0
+for i in range(1,n_gears+1):
+    if lst[i][0]==1:
+        ans += (1<<(i-1))
 print(ans)
